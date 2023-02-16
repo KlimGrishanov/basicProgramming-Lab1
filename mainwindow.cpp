fa8cfbff282266c8a987218c98e6a23d533f31fe
Гришанов Клим <klim.grishanov@mail.ru>
2023-02-16 03:41:33 +0300

    Ver 1.0
    Not refactoring code, but work

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QMessageBox>
+#include <string>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -18,6 +21,12 @@ MainWindow::MainWindow(QWidget *parent)
     connect(ui->btn_clear, &QPushButton::clicked, this, &MainWindow::on_btn_clear_clicked);
     connect(ui->btn_dot, &QPushButton::clicked, this, &MainWindow::on_btn_dot_clicked);
     connect(ui->btn_del, &QPushButton::clicked, this, &MainWindow::on_btn_del_clicked);
+    connect(ui->btn_plus, &QPushButton::clicked, this, &MainWindow::on_btn_plus_clicked);
+    connect(ui->btn_minus, &QPushButton::clicked, this, &MainWindow::on_btn_minus_clicked);
+    connect(ui->btn_multiply, &QPushButton::clicked, this, &MainWindow::on_btn_multiply_clicked);
+    connect(ui->btn_divide, &QPushButton::clicked, this, &MainWindow::on_btn_divide_clicked);
+    connect(ui->btn_equals, &QPushButton::clicked, this, &MainWindow::on_btn_equls_clicked);
+    connect(ui->btn_change_sign, &QPushButton::clicked, this, &MainWindow::on_btn_change_sign_clicked);
 }
 
 MainWindow::~MainWindow()
@@ -43,28 +52,216 @@ void MainWindow::set_btn_dot_enabled(bool status){
 }
 
 void MainWindow::on_btn_numbers_clicked(QString number){
-    QString new_label = get_calc_label();
-    new_label = new_label + number;
-    set_calc_label(new_label);
+    QString label = get_calc_label();
+    if(label.length() == 1 && number == "0" && label == "0") {
+        // except;
+    } else if (isResult) {
+        set_calc_label(number);
+        isResult = false;
+    } else if (label.length() == 1 && label == "0") {
+        set_calc_label(number);
+    } else if(label.length() < STR_LENGTH) {
+        label = label + number;
+        set_calc_label(label);
+    }
 }
 
 void MainWindow::on_btn_clear_clicked(){
-    set_calc_label("");
+    set_calc_label("0");
+    last_action = EMPTY;
+    set_base_stylesheet();
     set_btn_dot_enabled(1);
+    isResult = false;
 }
 
 void MainWindow::on_btn_dot_clicked(){
     QString new_label = get_calc_label();
-    new_label = new_label + ".";
-    set_calc_label(new_label);
-    set_btn_dot_enabled(0);
+    if(isResult){
+        set_calc_label("0.");
+        isResult = false;
+    }else if(new_label.length() == 0){
+        set_calc_label("0.");
+    }else if(new_label.length() < STR_LENGTH){
+        new_label = new_label + ".";
+        set_calc_label(new_label);
+        set_btn_dot_enabled(0);
+    }
 }
 
 void MainWindow::on_btn_del_clicked(){
     QString new_label = get_calc_label();
-    if((new_label.toStdString())[new_label.length()-1] == '.'){
+    if(isResult){
+        set_calc_label("0");
+        set_btn_dot_enabled(1);
+    } else {
+        new_label.remove(-1, 1);
+        set_calc_label(new_label);
+    }
+}
+
+void MainWindow::action_in_calc_memory(double number) {
+    if(last_action == PLUS) {
+        //ui -> btn_plus -> setStyleSheet("QPushButton {background: #DF6521;border-radius: 10px;color: #FFFFFF;font-weight: 600;}QPushButton:hover {background-color: #FF7223;}");
+        calc_memory = calc_memory + number;
+    } else if (last_action == MINUS) {
+        //ui -> btn_minus -> setStyleSheet("QPushButton {background: #DF6521;border-radius: 10px;color: #FFFFFF;font-weight: 600;}QPushButton:hover {background-color: #FF7223;}");
+        calc_memory = calc_memory - number;
+    } else if (last_action == MULTIPLY) {
+        //ui -> btn_multiply -> setStyleSheet("QPushButton {background: #DF6521;border-radius: 10px;color: #FFFFFF;font-weight: 600;}QPushButton:hover {background-color: #FF7223;}");
+        calc_memory = calc_memory * number;
+    } else if (last_action == DIVIDE){
+        //ui -> btn_divide -> setStyleSheet("QPushButton {background: #DF6521;border-radius: 10px;color: #FFFFFF;font-weight: 600;}QPushButton:hover {background-color: #FF7223;}");
+        calc_memory = calc_memory / number;
+    }
+}
+
+void MainWindow::set_base_stylesheet(){
+    if(last_action == PLUS) {
+        ui -> btn_plus -> setStyleSheet("QPushButton {background: #DF6521;border-radius: 10px;color: #FFFFFF;font-weight: 600;}QPushButton:hover {background-color: #FF7223;}");
+    } else if (last_action == MINUS) {
+        ui -> btn_minus -> setStyleSheet("QPushButton {background: #DF6521;border-radius: 10px;color: #FFFFFF;font-weight: 600;}QPushButton:hover {background-color: #FF7223;}");
+    } else if (last_action == MULTIPLY) {
+        ui -> btn_multiply -> setStyleSheet("QPushButton {background: #DF6521;border-radius: 10px;color: #FFFFFF;font-weight: 600;}QPushButton:hover {background-color: #FF7223;}");
+    } else if (last_action == DIVIDE){
+        ui -> btn_divide -> setStyleSheet("QPushButton {background: #DF6521;border-radius: 10px;color: #FFFFFF;font-weight: 600;}QPushButton:hover {background-color: #FF7223;}");
+    } else {
+        ui -> btn_plus -> setStyleSheet("QPushButton {background: #DF6521;border-radius: 10px;color: #FFFFFF;font-weight: 600;}QPushButton:hover {background-color: #FF7223;}");
+        ui -> btn_minus -> setStyleSheet("QPushButton {background: #DF6521;border-radius: 10px;color: #FFFFFF;font-weight: 600;}QPushButton:hover {background-color: #FF7223;}");
+        ui -> btn_multiply -> setStyleSheet("QPushButton {background: #DF6521;border-radius: 10px;color: #FFFFFF;font-weight: 600;}QPushButton:hover {background-color: #FF7223;}");
+        ui -> btn_divide -> setStyleSheet("QPushButton {background: #DF6521;border-radius: 10px;color: #FFFFFF;font-weight: 600;}QPushButton:hover {background-color: #FF7223;}");
+    }
+}
+
+void MainWindow::on_btn_plus_clicked() {
+    QString label = get_calc_label();
+    ui->lbl_calc_line->setText("");
+    ui->btn_plus->setStyleSheet("QPushButton {background: #808080; border-radius: 10px;color: #FFFFFF;font-weight: 600;}");
+    double number = label.toDouble();
+    if(last_action == PLUS && label != ""){
+        action_in_calc_memory(number);
+    } else if (last_action == PLUS && label == ""){
+        // except
+    } else if(label == "" && last_action != EMPTY) {
+        set_base_stylesheet();
+    } else if(last_action == EMPTY) {
+        last_action = PLUS;
+        calc_memory = number;
+    } else {
+        set_base_stylesheet();
+        action_in_calc_memory(number);
+    }
+    set_btn_dot_enabled(1);
+    printf("%lf", calc_memory);
+    last_action = PLUS;
+}
+
+void MainWindow::on_btn_minus_clicked() {
+    QString label = get_calc_label();
+ui->lbl_calc_line->setText("");
+    ui->btn_minus->setStyleSheet("QPushButton {background: #808080; border-radius: 10px;color: #FFFFFF;font-weight: 600;}");
+    double number = label.toDouble();
+    if(last_action == MINUS && label != ""){
+        action_in_calc_memory(number);
+    }else if ((last_action == MINUS && label == "")){
+        // except
+    } else if(label == "" && last_action != EMPTY) {
+        set_base_stylesheet();
+    } else if(last_action == EMPTY) {
+        last_action = MINUS;
+        calc_memory = number;
+    } else {
+        set_base_stylesheet();
+        action_in_calc_memory(number);
+    }
+    printf("%lf", calc_memory);
+    set_btn_dot_enabled(1);
+    last_action = MINUS;
+}
+
+void MainWindow::on_btn_multiply_clicked() {
+    QString label = get_calc_label();
+    ui->lbl_calc_line->setText("");
+    ui->btn_multiply->setStyleSheet("QPushButton {background: #808080; border-radius: 10px;color: #FFFFFF;font-weight: 600;}");
+    double number = label.toDouble();
+    if(last_action == MULTIPLY && label != ""){
+        action_in_calc_memory(number);
+    } else if((last_action == MULTIPLY && label == "")){
+        // except
+    } else if(label == "" && last_action != EMPTY) {
+        set_base_stylesheet();
+    } else if(last_action == EMPTY) {
+        last_action = MULTIPLY;
+        calc_memory = number;
+    } else {
+        set_base_stylesheet();
+        action_in_calc_memory(number);
+    }
+    set_btn_dot_enabled(1);
+    printf("%lf", calc_memory);
+    last_action = MULTIPLY;
+}
+
+void MainWindow::on_btn_divide_clicked() {
+    QString label = get_calc_label();
+    ui->lbl_calc_line->setText("");
+    ui->btn_divide->setStyleSheet("QPushButton {background: #808080; border-radius: 10px;color: #FFFFFF;font-weight: 600;}");
+    double number = label.toDouble();
+    if(last_action == DIVIDE && label != ""){
+        action_in_calc_memory(number);
+    }else if((last_action == DIVIDE && label == "")){
+        // except
+    } else if(label == "" && last_action != EMPTY) {
+        set_base_stylesheet();
+    } else if(last_action == EMPTY) {
+        last_action = DIVIDE;
+        calc_memory = number;
+    } else {
+        set_base_stylesheet();
+        action_in_calc_memory(number);
+    }
+    printf("%lf", calc_memory);
+    set_btn_dot_enabled(1);
+    last_action = DIVIDE;
+}
+
+QString MainWindow::validation_label(QString str) {
+    if(str == "inf") {
+        str = "0";
+        QMessageBox::critical(this, "Error Message", "Stop ignore math:\nDivide by zero");
+    } else if(str == "nan") {
+        str = "0";
+    }
+    return str;
+}
+
+void MainWindow::on_btn_equls_clicked() {
+    QString label = get_calc_label();
+    double number = label.toDouble();
+    action_in_calc_memory(number);
+    last_action = EMPTY;
+    isResult = true;
+    QString new_label = QString().setNum(calc_memory, 'g', PRECISION);
+    new_label = validation_label(new_label);
+    set_base_stylesheet();
+    ui->lbl_calc_line->setText(new_label);
+    if(get_calc_label().contains(".")) {
+        set_btn_dot_enabled(0);
+    } else {
         set_btn_dot_enabled(1);
     }
-    new_label.remove(-1, 1);
+}
+
+void MainWindow::on_btn_change_sign_clicked(){
+    QString label = get_calc_label();
+    QString new_label;
+    if(label.contains("-")){
+        new_label = label.remove(0, 1);
+    } else if (label != "0"){
+        new_label = "-" + label;
+    } else {
+        new_label = "-";
+    }
     set_calc_label(new_label);
 }
+
+
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,17 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <string>
+
+#define STR_LENGTH 30
+#define EMPTY 0
+#define PRECISION 7
+
+#define PLUS 1
+#define MINUS 2
+#define MULTIPLY 3
+#define DIVIDE 4
+
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -16,13 +27,27 @@ public:
     ~MainWindow();
 
 private:
+    double calc_memory = EMPTY;
+    int last_action = EMPTY;
+    bool isResult = false;
+
     Ui::MainWindow *ui;
+
     QString get_calc_label();
+    QString validation_label(QString str);
+    void set_base_stylesheet();
+    void action_in_calc_memory(double number);
     void set_calc_label(QString new_label);
     void set_btn_dot_enabled(bool status);
+    void on_btn_change_sign_clicked();
     void on_btn_numbers_clicked(QString number);
     void on_btn_clear_clicked();
     void on_btn_dot_clicked();
     void on_btn_del_clicked();
+    void on_btn_multiply_clicked();
+    void on_btn_plus_clicked();
+    void on_btn_divide_clicked();
+    void on_btn_minus_clicked();
+    void on_btn_equls_clicked();
 };
 #endif // MAINWINDOW_H
